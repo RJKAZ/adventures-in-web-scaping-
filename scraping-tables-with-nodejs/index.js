b401961d99b196e0585d30270666183b18c02031
Richard Kazanowski <rjkaz@comcast.net>
2021-04-21 12:53:09 -0400

    scraping tables

--- scraping-tables-with-nodejs/index.js
+++ scraping-tables-with-nodejs/index.js
@@ -6,9 +6,21 @@ async function main() {
     'https://www.codingwithstefan.com/table-example/'
   );
   const $ = cheerio.load(result);
-  $('body > table > tbody > tr > td').each((index, element) => {
-    console.log($(element).text());
+  const scrapedRows = [];
+  //$('body > table > tbody > tr > td').each((index, element) => {
+  //  console.log($(element).text());
+  //});
+  $("body > table > tbody > tr").each((index, element) => {
+    const tds = $(element).find("td");
+    const company = $(tds[0]).text();
+    const contact = $(tds[1]).text();
+    const country = $(tds[2]).text();
+    const scrapedRow = { company, contact, country}
+    scrapedRows.push(scrapedRow);
   });
+  console.log(scrapedRows);
 }
 
 main();
+
+// left off at 2:32 mark in the video
