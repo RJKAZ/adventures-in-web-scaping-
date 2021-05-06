06530b963b109d813c897ac591f815594aaaa305
Richard Kazanowski <rjkaz@comcast.net>
2021-05-06 17:16:23 -0400

    finished 2nd scraper, but it doesn't work, need to troubleshoot

--- craigslist-scraper2/index.js
+++ craigslist-scraper2/index.js
@@ -3,14 +3,55 @@ const cheerio = require('cheerio');
 
 const url = "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof?";
 
-async function scrapeCraigslist() {
+const scrapeResults = [];
+
+
+async function scrapeJobHeader() {
   try {
     const htmlResult = await request.get(url);
-    console.log(htmlResult);
+    const $ = await cheerio.load(htmlResult);
+
+    $(".result-info").each((index, element) => {
+      const resultTitle = $(element).children(".result-title");
+      const title = resultTitle.text();
+      const url = resultTitle.attr("href");
+      const datePosted = new Date($(element).children("time").attr("datatime"));
+      const hood = $(element).find(".result-hood").text();
+
+      const scrapeResult = { title, url, datePosted, hood };
+      scrapeResults.push(scrapeResult);
+    });
+    return scrapeResults;
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 }
 
+async function scrapeDescription(jobsWithHeaders) {
+  return await Promise.all(
+    jobsWithHeaders.map(async(job) => {
+    try {
+      const htmlResult = await request.get(job.url);
+    const $ = await cheerio.load(htmlResult);
+    $(".print-qrcode-container").remove();
+    job.description = $("#postingbody").text();
+    job.address = $("div.mapaddress").text();
+    const compensationText = $(".attrgroup").children().first().text();
+    job.compensation = compensationText.replace("compensation: ", "");
+    
+    return job;
+    } catch (error) {
+      console.error(error);
+    }
+  })
+  );
+}
+
+async function scrapeCraigslist() {
+  const jobsWithHeaders = await scrapeJobHeader();
+  const jobsFullData = await scrapeDescription(jobsWithHeaders);
+  console.log(jobsFullData);
+  console.log(jobsFullData.length);
+}
 
 scrapeCraigslist();
