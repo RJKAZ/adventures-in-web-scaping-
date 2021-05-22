f1d8cfc75ae49cbe5427a83889b093878c46eb8d
RJ <rjkaz@comcast.net>
2021-05-22 14:01:10 -0400

    section 14 CSRF tokens

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Node JS - Check
 And the chrome extension jQuery Inject
 
 Its a chrome tool that will inject any website with Jquery since scraping will be easier using Jquery instead of vanilla javascript. 
+
+very useful site for future uses, bascily disposable emails 
+https://temp-mail.org/en/
---
+++ craigslistPuppeteer/index.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+
+async function main() {
+    try {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto("https://accounts.craigslist.org/login")
+        await page.type("input#inputEmailHandle", "rjkaz@comcast.net")
+        await page.type("input#inputPassword", "" ); // need to put my password in to make this work
+        await page.click("button#login");
+        await page.waitForNavigation();
+        await page.goto(
+            "https://accounts.craigslist.org/login/home?show_tab=drafts"
+        )
+        const content = await page.content();
+        const $ = await cheerio.load(content);
+        console.log($("body > article > section > fieldset > b").text());
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
